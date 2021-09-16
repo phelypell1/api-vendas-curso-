4a4368cd830c5d726f2efac096dd415c4807156d
phelype <phelype.dev@outlook.com>
2021-09-16 16:40:29 -0300

    Criando o serviço para redefiniçao de senha

---
+++ src/modules/Users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokensRespository';
+import EtherealMail from '@config/mail/EtherealMail';
+
+//Interce referente a entidade Procuct
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (!emailExists) {
+      throw new AppError('email does not exists.');
+    }
+
+    const token = await userTokenRepository.genereted(emailExists.id);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token}`,
+    });
+  }
+}
+
+export default SendForgotPasswordEmailService;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@module/products/routes/products.routes';
+import passwordRouter from '@module/Users/routes/password.routes';
 import sessionRouter from '@module/Users/routes/session.routes';
 import usersRouter from '@module/Users/routes/users.routes';
 import { Router } from 'express';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/user', usersRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
