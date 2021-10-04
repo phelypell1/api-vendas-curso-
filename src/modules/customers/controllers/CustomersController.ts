08a493a79e48cbeb41e855ceaa7d1eb0e9a94271
phelype <phelype.dev@outlook.com>
2021-10-03 21:29:21 -0300

    Adicionado controller e router para Customer

--- src/modules/customers/controllers/CustomersController.ts
+++ src/modules/customers/controllers/CustomersController.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from 'express';
 import CreateCustomerService from '../services/CreateCustomerService';
+import DeleteCustomerService from '../services/DeleteCustomerService';
 import ListCustomersService from '../services/ListCustomerService';
+import ShowCustomerService from '../services/ShowCustomerService';
+import UpdateCustomerService from '../services/UpadateCustomerService';
 
 class CustomersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listCustomers = new ListCustomersService();
 
     const customers = await listCustomers.execute();
+
+    return response.json(customers);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const customerShow = new ShowCustomerService();
+
+    const customers = await customerShow.execute({ id });
 
     return response.json(customers);
   }
@@ -21,6 +34,27 @@ class CustomersController {
       email,
     });
     return response.json(customers);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const customersUpdate = new UpdateCustomerService();
+
+    const customers = await customersUpdate.execute({
+      id,
+      name,
+      email,
+    });
+    return response.json(customers);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const deleteCustomer = new DeleteCustomerService();
+    await deleteCustomer.execute({ id });
+    return response.json([]);
   }
 }
 
--- src/modules/customers/routes/customers.routes.ts
+++ src/modules/customers/routes/customers.routes.ts
@@ -10,6 +10,16 @@ customersRouter.use(isAuthenticaded);
 
 customersRouter.get('/', customersController.index);
 
+customersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.show,
+);
+
 customersRouter.post(
   '/',
   celebrate({
@@ -19,6 +29,30 @@ customersRouter.post(
     },
   }),
   customersController.create,
+);
+
+customersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.update,
+);
+
+customersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.delete,
 );
 
 export default customersRouter;
